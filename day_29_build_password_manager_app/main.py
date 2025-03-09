368c9a1c309ed42c5c2be3079c0bc265e3a3cc5b
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2025-03-09 12:39:22 +0200

    finished day 29, password generator

---
+++ day_29_build_password_manager_app/main.py
@@ -0,0 +1,91 @@
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from tkinter import *
+from tkinter import messagebox
+import random
+import string
+import pyperclip
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+# generate password function
+def generate_strong_password(length=15):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choices(characters, k=length))
+    return password
+
+
+# insert generated password to the input field function
+def insert_generated_password():
+    new_password = generate_strong_password()
+    password_entry.delete(0, END)
+    password_entry.insert(0, new_password)
+    pyperclip.copy(new_password)
+
+
+# add password to file function
+
+
+def add_password():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_entry.get()
+    if not website or not email or not password:
+        messagebox.showerror(
+            title="All fields are mandatory!",
+            message="Please fill website, email and password",
+        )
+        return
+    is_ok = messagebox.askokcancel(
+        title=website,
+        message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it okay to save?",
+    )
+    if is_ok:
+        new_entry = f"{website} | {email} | {password}"
+        with open(
+            "./day_29_build_password_manager_app/data.txt", mode="a"
+        ) as password_manager:
+            password_manager.write(f"\n{new_entry}")
+        website_input.delete(0, END)
+        password_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# canvas
+canvas = Canvas(height=200, width=200)
+lock_img = PhotoImage(file="./day_29_build_password_manager_app/logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+
+# labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# Entries
+website_input = Entry(width=35)
+website_input.grid(row=1, column=1, columnspan=2)
+website_input.focus()
+email_input = Entry(width=35)
+email_input.grid(column=1, row=2, columnspan=2)
+email_input.insert(0, "test@abv.bg")
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+# Buttons
+generate_password_button = Button(
+    text="Generate Password", command=insert_generated_password
+)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=add_password)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+window.mainloop()
